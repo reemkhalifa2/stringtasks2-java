185675c87f827035f03d10a2df12de5c253870c9
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-19 10:49:54 +0400

    Add Task2: countHi

--- src/countHi.java
+++ src/countHi.java
@@ -2,6 +2,7 @@ public class countHi {
     public static void main(String[] args) {
         System.out.println(countHi("hihi"));
     }
+
     public static Integer countHi(String str){
         Integer count =0;
         for(Integer i=0; i<str.length()-1;i++){
