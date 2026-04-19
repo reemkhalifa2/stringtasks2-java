e007f5ab963720e55eefc714463a3abb7a47e5a7
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-19 10:49:26 +0400

    Add Task1: doubleChar

--- src/doubleChar.java
+++ src/doubleChar.java
@@ -2,6 +2,7 @@ public class doubleChar {
     public static void main(String[] args) {
         System.out.println(doubleChar("The"));
     }
+
     public static String doubleChar(String str){
         String result="";
         for(Integer i=0; i<str.length();i++){
