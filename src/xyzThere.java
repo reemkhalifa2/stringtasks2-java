f460e01974790ac52c1b4d47c01ef22003906105
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-19 11:20:09 +0400

    Add Tas;6: xyzThere

---
+++ src/xyzThere.java
@@ -0,0 +1,15 @@
+public class xyzThere {
+    public static void main(String[] args) {
+        System.out.println(xyzThere("abc.xyz"));
+    }
+    public static Boolean xyzThere(String str){
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("xyz")) {
+                if (i == 0 || str.charAt(i - 1) != '.') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
