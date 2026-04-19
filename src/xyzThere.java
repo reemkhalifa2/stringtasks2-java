01bbc0c0f4be2d6ed4bfa5c46677579aaa4eb1aa
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-19 11:20:25 +0400

    Add Task6: xyzThere

--- src/xyzThere.java
+++ src/xyzThere.java
@@ -2,6 +2,7 @@ public class xyzThere {
     public static void main(String[] args) {
         System.out.println(xyzThere("abc.xyz"));
     }
+
     public static Boolean xyzThere(String str){
         for (int i = 0; i < str.length() - 2; i++) {
             if (str.substring(i, i + 3).equals("xyz")) {
