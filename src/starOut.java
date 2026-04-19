e3b1482e3f0cc1c85b1d6d96433e11453595e18a
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-19 14:29:23 +0400

    Add Task19: starOut

---
+++ src/starOut.java
@@ -0,0 +1,24 @@
+public class starOut {
+    public static void main(String[] args) {
+
+    }
+    public static String starOut(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == '*') {
+
+            }
+            else if (i > 0 && str.charAt(i - 1) == '*') {
+            }
+            else if (i < str.length() - 1 && str.charAt(i + 1) == '*') {
+            }
+            else {
+                result += str.charAt(i);
+            }
+        }
+
+        return result;
+    }
+}
